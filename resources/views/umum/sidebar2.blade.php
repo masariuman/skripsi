26ac405503cdcee328a2f09a632e9b62dfe61523
masariuman <masariuman@gmail.com>
2018-02-02 09:10:41 +0700

    sidebar

--- resources/views/umum/sidebar2.blade.php
+++ resources/views/umum/sidebar2.blade.php
@@ -38,45 +38,20 @@
                 </li>
                 <li>
                     <a href="{{url('admin/kontak')}}" class="{{ active('admin/kontak') }}" >
-                        <i class="fa fa-bullhorn"></i>
+                        <i class="fa fa-mobile"></i>
                         <span>Kontak</span>
                     </a>
                 </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class=" fa fa-bar-chart-o"></i>
-                        <span>Charts</span>
+                <li>
+                    <a href="{{url('admin/pengaturan')}}" class="{{ active('admin/pengaturan') }}" >
+                        <i class="fa fa-cog"></i>
+                        <span>Pengaturan</span>
                     </a>
-                    <ul class="sub">
-                        <li><a href="chartjs.html">Chart js</a></li>
-                        <li><a href="flot_chart.html">Flot Charts</a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class=" fa fa-bar-chart-o"></i>
-                        <span>Maps</span>
-                    </a>
-                    <ul class="sub">
-                        <li><a href="google_map.html">Google Map</a></li>
-                        <li><a href="vector_map.html">Vector Map</a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class="fa fa-glass"></i>
-                        <span>Extra</span>
-                    </a>
-                    <ul class="sub">
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="404.html">404 Error</a></li>
-                        <li><a href="registration.html">Registration</a></li>
-                    </ul>
                 </li>
                 <li>
-                    <a href="login.html">
-                        <i class="fa fa-user"></i>
-                        <span>Login Page</span>
+                    <a href="{{url('admin/logout')}}" class="{{ active('admin/logout') }}" >
+                        <i class="fa fa-sign-out"></i>
+                        <span>Keluar</span>
                     </a>
                 </li>
             </ul>   
--- storage/framework/views/306172a577d98726b1922e39cc9562fd82aec484.php
+++ storage/framework/views/306172a577d98726b1922e39cc9562fd82aec484.php
@@ -36,51 +36,22 @@
                         <li><a href="<?php echo e(url('admin/profil')); ?>" class="<?php echo e(active('admin/profil')); ?>" >Tambah Profil</a></li>
                     </ul>
                 </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class="fa fa-envelope"></i>
+                <li>
+                    <a href="<?php echo e(url('admin/kontak')); ?>" class="<?php echo e(active('admin/kontak')); ?>" >
+                        <i class="fa fa-mobile"></i>
                         <span>Kontak</span>
                     </a>
-                    <ul class="sub">
-                        <li><a href="mail.html">Inbox</a></li>
-                        <li><a href="mail_compose.html">Compose Mail</a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class=" fa fa-bar-chart-o"></i>
-                        <span>Charts</span>
-                    </a>
-                    <ul class="sub">
-                        <li><a href="chartjs.html">Chart js</a></li>
-                        <li><a href="flot_chart.html">Flot Charts</a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class=" fa fa-bar-chart-o"></i>
-                        <span>Maps</span>
-                    </a>
-                    <ul class="sub">
-                        <li><a href="google_map.html">Google Map</a></li>
-                        <li><a href="vector_map.html">Vector Map</a></li>
-                    </ul>
-                </li>
-                <li class="sub-menu">
-                    <a href="javascript:;">
-                        <i class="fa fa-glass"></i>
-                        <span>Extra</span>
-                    </a>
-                    <ul class="sub">
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="404.html">404 Error</a></li>
-                        <li><a href="registration.html">Registration</a></li>
-                    </ul>
                 </li>
                 <li>
-                    <a href="login.html">
-                        <i class="fa fa-user"></i>
-                        <span>Login Page</span>
+                    <a href="<?php echo e(url('admin/pengaturan')); ?>" class="<?php echo e(active('admin/pengaturan')); ?>" >
+                        <i class="fa fa-cog"></i>
+                        <span>Pengaturan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo e(url('admin/logout')); ?>" class="<?php echo e(active('admin/logout')); ?>" >
+                        <i class="fa fa-sign-out"></i>
+                        <span>Keluar</span>
                     </a>
                 </li>
             </ul>   
