f8204daa6c9262bb989bac0758e4d9bc965dd917
masariuman <masariuman@gmail.com>
2018-02-09 08:06:58 +0700

    Auto-committing 'welcome.blade.php'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel >> NGETeST SUBLIME AUTOCOMMIT AT ALL.
                 </div>
 
                 <div class="links">
